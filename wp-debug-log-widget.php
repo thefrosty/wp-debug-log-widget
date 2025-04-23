96394787bd89d66da7ec9620f27bce32788a4ebc
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-04-23 09:27:40 -0700

    Set version back to 1.4.5 until release.

--- wp-debug-log-widget.php
+++ wp-debug-log-widget.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
  * Description: Adds an admin dashboard widget to parse the WordPress error log file.
  * Author: Austin Passy
  * Author URI: https://github.com/thefrosty
- * Version: 1.5.0
+ * Version: 1.4.5
  * Requires at least: 6.6
  * Tested up to: 6.8.0
  * Requires PHP: 8.3
